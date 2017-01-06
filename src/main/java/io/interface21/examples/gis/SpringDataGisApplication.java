8b9486161e71b8cce634711b1ac1ab0d29d8434b
Heiko Scherrer <scherrer@openwms.org>
2017-01-06 20:48:05 +0100

    initial commit

---
+++ src/main/java/io/interface21/examples/gis/SpringDataGisApplication.java
@@ -0,0 +1,12 @@
+package io.interface21.examples.gis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataGisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataGisApplication.class, args);
+	}
+}
